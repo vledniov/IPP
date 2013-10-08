0b4ad8521e52110e7d4c23706e7233cb1a73334a
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-10-08 08:39:07 +0300

    Add localization git

---
+++ app/controllers/downloads_controller.rb
@@ -0,0 +1,10 @@
+class DownloadsController < ApplicationController
+  def index
+  end
+
+  def download
+  end
+
+  def second_page
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 IPP::Application.routes.draw do
 
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get '/' => "downloads#index"
+    get '/second_page' => "downloads#second_page"
+  end
+
+  match "*path", to: redirect("/#{I18n.default_locale}/%{path}"), via: [:get, :post]
+  match "", to: redirect("/#{I18n.default_locale}"), via: [:get, :post]
 end
