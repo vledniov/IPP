da13e7ec4326f8011d473002515906bad7a58f7e
Vladislav Ledniov <vlad.ledniov@gmail.com>
2014-01-29 18:08:25 +0200

    Prepare configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 IPP::Application.routes.draw do
-  resources :rates, only: :index
+  root to: "rates#index"
+  resources :rates, only: :index do
+    collection do
+      [:generate_rates, :generate_rates_with_proxy, :generate_caching_rates].each do |route|
+        post route
+      end
+    end
+  end
 end
