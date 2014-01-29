ffd28a8a40f91306398c912ebd41da73f1228b4d
Vladislav Ledniov <vlad.ledniov@gmail.com>
2014-01-29 18:08:55 +0200

    Implement rates controller

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,5 +1,23 @@
 class RatesController < ApplicationController
   def index
-    @rates = Rate.all
+  end
+
+  def generate_rates
+    Rate.destroy_all
+    1000.times do |i|
+      Rate.create!(made_on: Date.today - i, currency_code: "MDL", value: (0.1 + i/100 ))
+    end
+    redirect_to rates_path
+  end
+
+  def generate_rates_with_proxy
+    Rate.destroy_all
+    Generators::Rates.new(params[:part].to_i).generate
+    redirect_to rates_path
+  end
+
+  def generate_caching_rates
+    Generators::Rates.new.generate_with_cache
+    redirect_to rates_path
   end
 end
