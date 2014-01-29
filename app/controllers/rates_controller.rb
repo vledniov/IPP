ed4aa18e11e4da95e645c564b9abca8a35a13d83
Vladislav Ledniov <vlad.ledniov@gmail.com>
2014-01-29 18:26:50 +0200

    Some minor improvements, showing updates

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,23 +1,30 @@
 class RatesController < ApplicationController
   def index
+    @time = params[:time]
   end
 
   def generate_rates
     Rate.destroy_all
+    time_now = Time.now
     1000.times do |i|
       Rate.create!(made_on: Date.today - i, currency_code: "MDL", value: (0.1 + i/100 ))
     end
-    redirect_to rates_path
+    time_then = Time.now
+    redirect_to rates_path(time: (time_then - time_now).seconds)
   end
 
   def generate_rates_with_proxy
     Rate.destroy_all
+    time_now = Time.now
     Generators::Rates.new(params[:part].to_i).generate
-    redirect_to rates_path
+    time_then = Time.now
+    redirect_to rates_path(time: (time_then - time_now).seconds)
   end
 
   def generate_caching_rates
+    time_now = Time.now
     Generators::Rates.new.generate_with_cache
-    redirect_to rates_path
+    time_then = Time.now
+    redirect_to rates_path(time: (time_then - time_now).seconds)
   end
 end
