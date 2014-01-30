aa5fb5f444d9a1f467691d7f5ed657458d65e015
Vladislav Ledniov <vlad.ledniov@gmail.com>
2014-01-30 13:25:39 +0200

    Clear the project

--- config/environment.rb
+++
@@ -1,6 +0,0 @@
-# Load the Rails application.
-require 'pp'
-require File.expand_path('../application', __FILE__)
-
-# Initialize the Rails application.
-IPP::Application.initialize!
--- config/routes.rb
+++
@@ -1,3 +0,0 @@
-IPP::Application.routes.draw do
-
-end
